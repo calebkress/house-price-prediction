092e036d9917487ce268463fed1fb6921dd06fc3
Caleb Kress <calebkress@gmail.com>
2023-09-19 18:40:17 -0500

    added some comments, removed some unnecessary clutter

--- house-prediction/src/App.js
+++ house-prediction/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import Home from './Home';
 
 
 function App() {
   return (
+    // This router governs the high-level navigation of the app
+    // Longterm we'll need links for the various segments we're implementing
+    // It may make sense for some Routes to occur on the same page, e.g.
+    // having the visualizations render once the user clicks "Calculate"
     <BrowserRouter>
       <div className="appDiv">
         <Routes>
