d0fd7714c3c16015d4c39b2d212c4349b2034721
Caleb Kress <calebkress@gmail.com>
2023-10-31 23:42:10 -0500

    updated favicon and site title

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import houseIcon from './static/house.jpeg';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,5 +11,12 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// Dynamically set the favicon
+const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+link.type = 'image/jpeg';  // Update the type to match the image type
+link.rel = 'shortcut icon';
+link.href = houseIcon;
+document.getElementsByTagName('head')[0].appendChild(link);
 
 reportWebVitals();
